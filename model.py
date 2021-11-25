be2083ebd4623373b7e0628c1864b8612cf8928b
Milan Tancak <tancak.milan@gmail.com>
2021-11-25 00:46:36 +0000

    fixed the dimensions in the layers so that the input is passed in with shape of (token_count, embedding_size, 1)

--- model.py
+++ model.py
@@ -19,31 +19,35 @@ class SelfAttention(nn.Module):
         print("len_head = " + str(self.len_head))
 
         self.WK = nn.Conv1d(
-            in_channels=num_embeddings,
-            out_channels=num_heads * num_embeddings,
-            kernel_size=(1)
+            in_channels=self.len_embedding,
+            out_channels=num_heads * self.len_head,
+            kernel_size=1
         )
         self.WQ = nn.Conv1d(
-            in_channels=num_embeddings,
-            out_channels=num_heads * num_embeddings,
-            kernel_size=(1)
+            in_channels=self.len_embedding,
+            out_channels=num_heads * self.len_head,
+            kernel_size=1
         )
         self.WV = nn.Conv1d(
-            in_channels=num_embeddings,
-            out_channels=num_heads * num_embeddings,
-            kernel_size=(1)
+            in_channels=self.len_embedding,
+            out_channels=num_heads * self.len_head,
+            kernel_size=1
         )
 
         self.WZ = nn.Conv1d(
-            in_channels=num_heads * num_embeddings,
-            out_channels=num_embeddings,
-            kernel_size=(1)
+            in_channels=num_heads * self.len_head,
+            out_channels=len_embedding,
+            kernel_size=1
         )
 
-    def forward(self, embeddings):
-        K = self.WK(embeddings).reshape(self.num_heads, self.num_embeddings, self.len_embedding).squeeze()
-        Q = self.WQ(embeddings).reshape(self.num_heads, self.num_embeddings, self.len_embedding).squeeze()
-        V = self.WV(embeddings).reshape(self.num_heads, self.num_embeddings, self.len_embedding).squeeze()
+    def forward(self, input):
+        print("input shape = " + str(input.shape))
+        K = self.WK(input)
+        print("K1 shape = " + str(K.shape))
+        K = K.T.reshape(self.num_heads, self.num_embeddings, self.len_head).squeeze()
+        print("K2 shape = " + str(K.shape))
+        Q = self.WQ(input).reshape(self.num_heads, self.num_embeddings, self.len_head).squeeze()
+        V = self.WV(input).reshape(self.num_heads, self.num_embeddings, self.len_head).squeeze()
 
         print("k shape = " + str(K.shape))
         print("q shape = " + str(Q.shape))
@@ -56,8 +60,12 @@ class SelfAttention(nn.Module):
         indexes = torch.softmax(score / self.len_head, dim=2)
         print("indexes shape = " + str(indexes.shape))
 
-        Z = indexes.bmm(V).reshape(1, 400, 256)
-        print("Z shape = " + str(Z.shape))
+        Z = indexes.bmm(V)
+        print("Z1 shape = " + str(Z.shape))
+        Z = Z.moveaxis(2, 1)
+        print("Z2 shape = " + str(Z.shape))
+        Z = Z.flatten(start_dim=0, end_dim=1).T.unsqueeze(dim=2)
+        print("Z3 shape = " + str(Z.shape))
 
         output = self.WZ(Z)
         return output
@@ -84,9 +92,9 @@ if __name__ == "__main__":
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     print(DEVICE)
 
-    x = torch.rand(1, 50, 256).to(DEVICE)
+    x = torch.rand(50, 256, 1).to(DEVICE)
     print(x.shape)
 
     sa = SelfAttention().to(DEVICE)
     pred = sa(x)
-    print(pred.shape)
+    print("output shape = " + str(pred.shape))
